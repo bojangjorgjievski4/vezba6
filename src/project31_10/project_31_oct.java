272fcb1db93ce129449d6897b3632fcd88c4fe8f
bojangjorgjievski4 <bojangjorgjievski399@gmail.com>
2023-11-30 10:02:35 +0100

    first commit

---
+++ src/project31_10/project_31_oct.java
@@ -0,0 +1,42 @@
+package project31_10;
+
+public class project_31_oct {
+
+public static void main(String[] args) {
+
+int age = 25;
+
+if(age > 0)
+
+{
+
+if( age > 16 )
+
+{
+
+System.out.println("Welcome!");
+
+}
+
+else
+
+{
+
+System.out.println("Too Young !");
+
+}
+
+
+}
+
+else
+
+{
+
+System.out.println("Error");
+
+}
+
+}
+
+}
